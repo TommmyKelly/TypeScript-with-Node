785c791c42a7f30dd5f86c7aed194bf78c12fcd1
TommmyKelly <tommy_kelly@icloud.com>
2021-03-19 01:39:51 +0000

    Add: Added index.html and 404.html page

--- dist/server.js
+++ dist/server.js
@@ -4,14 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const user = require("./routes/user");
-const add = (num1, num2) => {
-    return num1 + num2;
-};
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "public", "index.html"));
+});
 app.get("/:id", (req, res) => {
     console.log(req.params.id);
-    res.send("hello");
+    res.send("id");
 });
 app.use("/user", user);
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "public", "404.html"));
+});
 app.listen(5000, () => console.log("Running on 5000"));
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,34 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import { dirname } from "node:path";
+import { REPLCommand } from "node:repl";
+import path from "path";
 
 const app: Application = express();
 const user = require("./routes/user");
 
-const add = (num1: number, num2: number): number => {
-  return num1 + num2;
-};
+app.use(express.static(path.join(__dirname, "public")));
+
+// const add = (num1: number, num2: number): number => {
+//   return num1 + num2;
+// };
 
 interface Props {
   name: string;
   email: string;
 }
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/:id", (req: Request, res: Response) => {
   console.log(req.params.id);
 
-  res.send("hello");
+  res.send("id");
 });
-
 app.use("/user", user);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "public", "404.html"));
+});
 
 app.listen(5000, () => console.log("Running on 5000"));
